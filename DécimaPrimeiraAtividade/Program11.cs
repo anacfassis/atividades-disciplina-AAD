5e548c840a84d1a403beb5d6c029f42c6d0a8fec
Ana Carolina Fernandes de Assis <assiscarolina18@gmail.com>
2023-05-31 23:09:53 -0300

    Incluindo duas atividades
    Incluindo as atividades 11ª e 12ª

---
+++ DécimaPrimeiraAtividade/Program11.cs
@@ -0,0 +1,95 @@
+﻿class Eq2Grau                           //Classe para trabalhar com uma Equação do 2º grau
+{
+    private double _a;
+
+    public double a
+    {
+        get { return _a; }
+        set { _a = value; }
+    }
+
+    private double _b;
+
+    public double b
+    {
+        get { return _b; }
+        set { _b = value; }
+    }
+
+    private double _c;
+
+    public double c
+    {
+        get { return _c; }
+        set { _c = value; }
+    }
+
+    private double _Delta;
+
+    public double Delta
+    {
+        get { return _Delta; }
+        //set { _Delta = value; }                   //Usuário não terá acesso para alterar o valor de Delta
+    }
+
+    private double _Raiz1;
+
+    public double Raiz1
+    {
+        get { return _Raiz1; }
+        //set { _Raiz1 = value; }                   //Usuário não terá acesso para alterar o valor de Raiz1
+    }
+
+    private double _Raiz2;
+
+    public double Raiz2
+    {
+        get { return _Raiz2; }
+        //set { _Raiz2 = value; }                   //Usuário não terá acesso para alterar o valor de Raiz2
+    }
+
+    public void CalcDelta()
+    {
+        _Delta = Math.Pow(b,2) - 4 * a * c;
+    }
+
+    public void CalcRaízes()
+    {
+        _Raiz1 = (-b + Math.Sqrt(Delta)) / (2 * a);
+        _Raiz2 = (-b - Math.Sqrt(Delta)) / (2 * a);
+    }
+}
+
+class Program11
+{
+    static void Main(string[] args)
+    {
+        Eq2Grau Equação = new Eq2Grau();
+
+        Console.Write("a =  ");
+        Equação.a = double.Parse(Console.ReadLine());
+
+        Console.Write("b =  ");
+        Equação.b = double.Parse(Console.ReadLine());
+
+        Console.Write("c =  ");
+        Equação.c = double.Parse(Console.ReadLine());
+
+        Equação.CalcDelta();                          // Calcula o delta
+
+        Console.WriteLine($"\nDelta = {Equação.Delta:F2}\n");
+
+        if (Equação.Delta >= 0)
+        {
+            Equação.CalcRaízes();                     // Calcula as raízes
+
+            Console.WriteLine($"Raiz 1 = {Equação.Raiz1:F2}");
+            Console.WriteLine($"Raiz 2 = {Equação.Raiz2:F2}");
+        } else
+        {
+            Console.WriteLine("A equação não possui raízes reais.");
+        }
+
+        Console.ReadKey();
+    }
+}
---
+++ DécimaSegundaAtividade/Program12.cs
@@ -0,0 +1,88 @@
+﻿//Faça um programa que defina de forma completa uma classe de Nome “Veículo” com os atributos “Modelo”, “Marca”, “Ano de Fabricação” e “Placa”. Crie métodos capazes de ler os dados de um veículo e mostrar uma listagem de todos os veículos.
+//Cadastre em um vetor 30 veículos.
+
+class Veículo
+{
+    private string _Modelo;
+    public string Modelo
+    {
+        get { return _Modelo; }
+        set { _Modelo = value; }
+    }
+
+    private string _Marca;
+
+    public string Marca
+    {
+        get { return _Marca; }
+        set { _Marca = value; }
+    }
+
+    private string _AnoFabricação;
+
+    public string AnoFabricação
+    {
+        get { return _AnoFabricação; }
+        set { _AnoFabricação = value; }
+    }
+
+    private string _Placa;
+
+    public string Placa
+    {
+        get { return _Placa; }
+        set { _Placa = value; }
+    }
+
+    public void CadastrarVeículo()
+    {
+        Console.Clear();
+
+            Console.Write($"\nModelo do veículo: ");
+            _Modelo = Console.ReadLine();
+
+            Console.Write("Marca do veículo: ");
+            _Marca = Console.ReadLine();
+
+            Console.Write("Ano de fabricação do veículo: ");
+            _AnoFabricação = Console.ReadLine();
+
+            Console.Write("Placa do veículo: ");
+            _Placa = Console.ReadLine();
+    }
+
+    public void ListaDados()
+    {
+        Console.WriteLine($"\n{_Marca} {_Modelo}");
+        Console.WriteLine($"Placa {_Placa} - Ano de fabricação: {_AnoFabricação}");
+    }
+
+}
+
+class Program12
+{
+    static void Main(string[] args)
+    {
+        Veículo[] veículos = new Veículo[3];            //Vetor de 3 veículos como exemplo
+
+        for (int i = 0; i < 3; i++)
+        {
+            Veículo x = new Veículo();                 //Instanciando um objeto da classe Veículo
+                                                       // x é um objeto e sabe ler os dados de um veículo...
+            x.CadastrarVeículo();
+                                                      // Nesse momento x já possui todos os dados lidos
+                                                     // Vamos armazenar no vetor...
+            veículos[i] = x;
+        }
+        
+        Console.Clear();
+
+        foreach (Veículo V in veículos)
+        {
+                                                   // Para cada veículo V no cadastro...
+            V.ListaDados();
+        }
+        
+        Console.ReadKey();
+    }
+}
